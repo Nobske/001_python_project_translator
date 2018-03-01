73c1de270dbade4c18f063475597e9a57435ba86
Norbert Adam <nobske800@gmail.com>
2018-03-01 13:30:31 +0100

    first commit

---
+++ AY_Translator.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 12:00:17 2018
+
+@author: Norbert Adam
+"""
+
+pyg = 'ay'
+
+original = input('Enter a word:') #input für die Abfrage
+
+if len(original) > 0 and original.isalpha(): #über 0 und ob es text ist
+    word = original.lower() #wird in nur lowercases umgewandelt
+    first = word[0] #erstes Zeichen 
+    new_word = word + first + pyg #wort +erste zeichen +ay
+    new_word = new_word[1:] # ohne erste Zeichen
+    print (new_word)
+else:
+    print ('empty')
+
+
+garbled = "!XeXgXaXsXsXeXmX XtXeXrXcXeXsX XeXhXtX XmXaX XI"
+message = garbled[::-2]
+
+print (message)
+#i am the secret message
